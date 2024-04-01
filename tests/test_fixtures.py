32fe0e69ba570aa7b340f975ad341e550b29da4b
Irina.Voron <yarutich.irina@gmail.com>
2024-04-01 17:33:41 +0300

    Add test_fixtures.py

---
+++ tests/test_fixtures.py
@@ -0,0 +1,16 @@
+"""
+Сделайте разные фикстуры для каждого теста, которые выставят размеры окна браузера
+"""
+
+from selene import browser
+
+
+def test_github_desktop(desktop):
+    browser.open('/')
+    browser.element('.HeaderMenu-link--sign-in').click()
+
+
+def test_github_mobile(mobile):
+    browser.open('/')
+    browser.element('.Button--link').click()
+    browser.element('.HeaderMenu-link--sign-in').click()

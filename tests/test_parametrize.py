c9779d999e38345bc4253660630aab120f480fcd
Irina.Voron <yarutich.irina@gmail.com>
2024-04-01 18:09:08 +0300

    Add variables desktop/mobile_only

--- tests/test_parametrize.py
+++ tests/test_parametrize.py
@@ -14,17 +14,22 @@ def browser_settings(request):
     browser.config.window_height = window_height
 
 
-@pytest.mark.parametrize('browser_settings', [(1920, 1080), (1366, 768), (1536, 864)],
-                         ids=['large', 'medium', 'small'],
-                         indirect=True)
+desktop_only = pytest.mark.parametrize('browser_settings', [(1920, 1080), (1366, 768), (1536, 864)],
+                                       ids=['large', 'medium', 'small'],
+                                       indirect=True)
+
+mobile_only = pytest.mark.parametrize('browser_settings', [(360, 780), (390, 844), (414, 896)],
+                                      ids=['iPhone 12 mini', 'iPhone 12 Pro', 'iPhone XR'],
+                                      indirect=True)
+
+
+@desktop_only
 def test_github_desktop(browser_settings):
     browser.open('/')
     browser.element('.HeaderMenu-link--sign-in').click()
 
 
-@pytest.mark.parametrize('browser_settings', [(360, 780), (390, 844), (414, 896)],
-                         ids=['iPhone 12 mini', 'iPhone 12 Pro', 'iPhone XR'],
-                         indirect=True)
+@mobile_only
 def test_github_mobile(browser_settings):
     browser.open('/')
     browser.element('.Button--link').click()

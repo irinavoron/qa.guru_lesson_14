9e20edc3dfa9997e7ed968aff5d7fad15ee2ef52
Irina.Voron <yarutich.irina@gmail.com>
2024-04-01 18:00:11 +0300

    Add test_parametrize.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,8 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture(params=[(1920, 1080), (1366, 768), (1536, 864)], ids=['large', 'medium', 'small'])
+@pytest.fixture(params=[(1920, 1080), (1366, 768), (1536, 864)],
+                ids=['large', 'medium', 'small'])
 def desktop(request):
     browser.config.base_url = 'https://github.com'
     window_width, window_height = request.param
@@ -10,7 +11,8 @@ def desktop(request):
     browser.config.window_height = window_height
 
 
-@pytest.fixture(params=[(360, 780), (390, 844), (414, 896)], ids=['iPhone 12 mini', 'iPhone 12 Pro', 'iPhone XR'])
+@pytest.fixture(params=[(360, 780), (390, 844), (414, 896)],
+                ids=['iPhone 12 mini', 'iPhone 12 Pro', 'iPhone XR'])
 def mobile(request):
     browser.config.base_url = 'https://github.com'
     window_width, window_height = request.param
--- tests/test_parametrize.py
+++ tests/test_parametrize.py
@@ -2,16 +2,30 @@
 Переопределите параметр с помощью indirect параметризации на уровне теста
 """
 import pytest
+from selene import browser
 
 
-@pytest.fixture()
-def browser():
-    pass
+@pytest.fixture(params=[(1920, 1080), (1366, 768), (1536, 864), (360, 780), (390, 844), (414, 896)],
+                ids=['large', 'medium', 'small', 'iPhone 12 mini', 'iPhone 12 Pro', 'iPhone XR'])
+def browser_settings(request):
+    browser.config.base_url = 'https://github.com'
+    window_width, window_height = request.param
+    browser.config.window_width = window_width
+    browser.config.window_height = window_height
 
 
-def test_github_desktop(browser):
-    pass
+@pytest.mark.parametrize('browser_settings',[(1920, 1080), (1366, 768), (1536, 864)],
+                         ids=['large', 'medium', 'small'],
+                         indirect=True)
+def test_github_desktop(browser_settings):
+    browser.open('/')
+    browser.element('.HeaderMenu-link--sign-in').click()
 
 
-def test_github_mobile(browser):
-    pass
+@pytest.mark.parametrize('browser_settings', [(360, 780), (390, 844), (414, 896)],
+                         ids=['iPhone 12 mini', 'iPhone 12 Pro', 'iPhone XR'],
+                         indirect=True)
+def test_github_mobile(browser_settings):
+    browser.open('/')
+    browser.element('.Button--link').click()
+    browser.element('.HeaderMenu-link--sign-in').click()

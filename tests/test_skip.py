255bb1ae3b7318ac38f88c885f691b8d337b2b51
Irina.Voron <yarutich.irina@gmail.com>
2024-04-07 20:38:46 +0300

    Add test_skip.py

--- tests/test_skip.py
+++ tests/test_skip.py
@@ -3,16 +3,21 @@
 Пропустите мобильный тест, если соотношение сторон десктопное (и наоборот)
 """
 import pytest
+from selene import browser
 
 
-@pytest.fixture()
-def browser():
-    pass
+def test_github_desktop(browser_settings):
+    if browser_settings == 'mobile':
+        pytest.skip('This is mobile resolution')
+    else:
+        browser.open('/')
+        browser.element('.HeaderMenu-link--sign-in').click()
 
 
-def test_github_desktop(browser):
-    pass
-
-
-def test_github_mobile(browser):
-    pass
+def test_github_mobile(browser_settings):
+    if browser_settings == 'desktop':
+        pytest.skip('This is desktop resolution')
+    else:
+        browser.open('/')
+        browser.element('.Button-content').click()
+        browser.element('.HeaderMenu-link--sign-in').click()

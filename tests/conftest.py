0f2e27945108528e1dadb7b00b1f9e4391ef09e0
Irina.Voron <yarutich.irina@gmail.com>
2024-04-07 14:51:19 +0300

    Add test_fixtures.py

---
+++ tests/conftest.py
@@ -0,0 +1,33 @@
+import pytest
+from selene import browser
+
+
+base_url = 'https://github.com/'
+@pytest.fixture(
+    params=[(1920, 1080), (1366, 768), (1536, 864)],
+    ids=['large', 'medium', 'small']
+)
+def desktop_browser_settings(request):
+    browser.config.base_url = base_url
+    window_width, window_height = request.param
+    browser.config.window_width = window_width
+    browser.config.window_height = window_height
+
+    yield
+
+    browser.quit()
+
+
+@pytest.fixture(
+    params=[(360, 780), (390, 844), (414, 896)],
+    ids=['iPhone 12 mini', 'iPhone 12 Pro', 'iPhone XR']
+)
+def mobile_browser_settings(request):
+    browser.config.base_url = base_url
+    window_width, window_height = request.param
+    browser.config.window_width = window_width
+    browser.config.window_height = window_height
+
+    yield
+
+    browser.quit()
--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -1,11 +1,15 @@
 """
 Сделайте разные фикстуры для каждого теста, которые выставят размеры окна браузера
 """
+from selene import browser
 
 
-def test_github_desktop():
-    pass
+def test_github_desktop(desktop_browser_settings):
+    browser.open('/')
+    browser.element('.HeaderMenu-link--sign-in').click()
 
 
-def test_github_mobile():
-    pass
+def test_github_mobile(mobile_browser_settings):
+    browser.open('/')
+    browser.element('.Button-content').click()
+    browser.element('.HeaderMenu-link--sign-in').click()

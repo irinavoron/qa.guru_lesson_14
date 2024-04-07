044c647899ad170cf643d72e7ac8721ce6d54ae7
Irina.Voron <yarutich.irina@gmail.com>
2024-04-07 20:19:49 +0300

    Add test_parametrize.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,9 @@
 import pytest
 from selene import browser
 
-
 base_url = 'https://github.com/'
+
+
 @pytest.fixture(
     params=[(1920, 1080), (1366, 768), (1536, 864)],
     ids=['large', 'medium', 'small']
@@ -23,6 +24,21 @@ def desktop_browser_settings(request):
     ids=['iPhone 12 mini', 'iPhone 12 Pro', 'iPhone XR']
 )
 def mobile_browser_settings(request):
+    browser.config.base_url = base_url
+    window_width, window_height = request.param
+    browser.config.window_width = window_width
+    browser.config.window_height = window_height
+
+    yield
+
+    browser.quit()
+
+
+@pytest.fixture(
+    params=[(1920, 1080), (1366, 768), (1536, 864), (360, 780), (390, 844), (414, 896)],
+    ids=['large', 'medium', 'small', 'iPhone 12 mini', 'iPhone 12 Pro', 'iPhone XR']
+)
+def browser_settings(request):
     browser.config.base_url = base_url
     window_width, window_height = request.param
     browser.config.window_width = window_width
--- tests/test_parametrize.py
+++ tests/test_parametrize.py
@@ -2,16 +2,31 @@
 Переопределите параметр с помощью indirect параметризации на уровне теста
 """
 import pytest
+from selene import browser
+
+desktop_only = pytest.mark.parametrize(
+    argnames='browser_settings',
+    argvalues=[(1920, 1080), (1366, 768), (1536, 864)],
+    ids=['large', 'medium', 'small'],
+    indirect=True
+)
+
+mobile_only = pytest.mark.parametrize(
+    argnames='browser_settings',
+    argvalues=[(360, 780), (390, 844), (414, 896)],
+    ids=['iPhone 12 mini', 'iPhone 12 Pro', 'iPhone XR'],
+    indirect=True
+)
 
 
-@pytest.fixture()
-def browser():
-    pass
+@desktop_only
+def test_github_desktop(browser_settings):
+    browser.open('/')
+    browser.element('.HeaderMenu-link--sign-in').click()
 
 
-def test_github_desktop(browser):
-    pass
-
-
-def test_github_mobile(browser):
-    pass
+@mobile_only
+def test_github_mobile(browser_settings):
+    browser.open('/')
+    browser.element('.Button-content').click()
+    browser.element('.HeaderMenu-link--sign-in').click()

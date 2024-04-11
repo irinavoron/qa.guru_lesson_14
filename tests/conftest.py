f206593637e910f3370dd099bf3a16ee23e531b7
Irina.Voron <yarutich.irina@gmail.com>
2024-04-11 20:32:55 +0300

    Add selenoid

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,40 @@
+import os
 import pytest
 from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from dotenv import load_dotenv
 
 base_url = 'https://github.com/'
+
+
+@pytest.fixture(autouse=True)
+def selenoid_browser_settings():
+    load_dotenv()
+
+    options = Options()
+
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "122.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+
+    url = os.getenv('SELENOID_URL')
+    login = os.getenv('SELENOID_LOGIN')
+    password = os.getenv('SELENOID_PASSWORD')
+
+    driver = webdriver.Remote(
+        command_executor=f'https://{login}:{password}@{url}/wd/hub',
+        options=options)
+
+    browser.config.driver = driver
 
 
 @pytest.fixture(

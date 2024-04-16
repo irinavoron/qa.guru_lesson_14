5ccad472425266390210ada3daeb0841537cd45b
Irina.Voron <yarutich.irina@gmail.com>
2024-04-16 21:05:07 +0300

    Update test_fixtures.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,52 +1,5 @@
-import os
-
 import pytest
 from selene import browser
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from dotenv import load_dotenv
-
-import config
-from config import config
-from utils import attach
-
-
-@pytest.fixture(autouse=True)
-def selenoid_browser_settings():
-    load_dotenv()
-
-    options = Options()
-
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "122.0",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
-
-        }
-    }
-
-    options.capabilities.update(selenoid_capabilities)
-
-    selenoid_url = os.getenv('SELENOID_URL')
-    selenoid_login = os.getenv('SELENOID_LOGIN')
-    selenoid_password = os.getenv('SELENOID_PASSWORD')
-
-    driver = webdriver.Remote(
-        command_executor=f'https://{selenoid_login}:{selenoid_password}@{selenoid_url}/wd/hub',
-        options=options)
-
-    browser.config.driver = driver
-
-    yield
-
-    attach.add_screenshot(browser)
-    attach.add_html(browser)
-    attach.add_logs(browser)
-    attach.add_video(browser)
-
-    browser.quit()
 
 
 @pytest.fixture(
@@ -54,7 +7,7 @@ def selenoid_browser_settings():
     ids=['large', 'medium', 'small']
 )
 def desktop_browser_settings(request):
-    browser.config.base_url = config.BASE_URL
+    browser.config.base_url = 'https://github.com/'
     window_width, window_height = request.param
     browser.config.window_width = window_width
     browser.config.window_height = window_height
@@ -67,7 +20,7 @@ def desktop_browser_settings(request):
     ids=['iPhone 12 mini', 'iPhone 12 Pro', 'iPhone XR']
 )
 def mobile_browser_settings(request):
-    browser.config.base_url = config.BASE_URL
+    browser.config.base_url = 'https://github.com/'
     window_width, window_height = request.param
     browser.config.window_width = window_width
     browser.config.window_height = window_height
@@ -75,18 +28,3 @@ def mobile_browser_settings(request):
     yield
 
 
-@pytest.fixture(
-    params=[(1920, 1080), (1366, 768), (1536, 864), (360, 780), (390, 844), (414, 896)],
-    ids=['large', 'medium', 'small', 'iPhone 12 mini', 'iPhone 12 Pro', 'iPhone XR']
-)
-def browser_settings(request):
-    browser.config.base_url = config.BASE_URL
-    browser.config.timeout = config.TIMEOUT
-    window_width, window_height = request.param
-    browser.config.window_width = window_width
-    browser.config.window_height = window_height
-
-    if window_width < 800:
-        yield 'mobile'
-    else:
-        yield 'desktop'
--- tests/test_skip.py
+++ tests/test_skip.py
@@ -3,21 +3,16 @@
 Пропустите мобильный тест, если соотношение сторон десктопное (и наоборот)
 """
 import pytest
-from selene import browser
 
 
-def test_github_desktop(browser_settings):
-    if browser_settings == 'mobile':
-        pytest.skip('This is mobile resolution')
-    else:
-        browser.open('/')
-        browser.element('.HeaderMenu-link--sign-in').click()
+@pytest.fixture()
+def browser():
+    pass
 
 
-def test_github_mobile(browser_settings):
-    if browser_settings == 'desktop':
-        pytest.skip('This is desktop resolution')
-    else:
-        browser.open('/')
-        browser.element('.Button-content').click()
-        browser.element('.HeaderMenu-link--sign-in').click()
+def test_github_desktop(browser):
+    pass
+
+
+def test_github_mobile(browser):
+    pass

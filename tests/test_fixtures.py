d72966150cb631bd0fc481a396479b47c36b2631
Irina.Voron <yarutich.irina@gmail.com>
2024-04-17 14:03:28 +0300

    Add allure

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -1,15 +1,20 @@
 """
 Сделайте разные фикстуры для каждого теста, которые выставят размеры окна браузера
 """
+import allure
 from selene import browser
 
 
 def test_github_desktop(desktop_browser_settings):
-    browser.open('/')
-    browser.element('.HeaderMenu-link--sign-in').click()
+    with allure.step('Open main page'):
+        browser.open('/')
+    with allure.step('Open the SignIn form'):
+        browser.element('.HeaderMenu-link--sign-in').click()
 
 
 def test_github_mobile(mobile_browser_settings):
-    browser.open('/')
-    browser.element('.Button-content').click()
-    browser.element('.HeaderMenu-link--sign-in').click()
+    with allure.step('Open main page'):
+        browser.open('/')
+    with allure.step('Open the SignIn form'):
+        browser.element('.Button-content').click()
+        browser.element('.HeaderMenu-link--sign-in').click()

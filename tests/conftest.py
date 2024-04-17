f4c8e56ab22b8f5608888cfab9908bd864e94539
Irina.Voron <yarutich.irina@gmail.com>
2024-04-17 13:28:11 +0300

    Update parametrize.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,3 +28,16 @@ def mobile_browser_settings(request):
     yield
 
 
+@pytest.fixture(
+    params=[(1920, 1080), (1366, 768), (1536, 864), (360, 780), (390, 844), (414, 896)],
+    ids=['large', 'medium', 'small', 'iPhone 12 mini', 'iPhone 12 Pro', 'iPhone XR']
+)
+def browser_settings(request):
+    browser.config.base_url = 'https://github.com/'
+    window_width, window_height = request.param
+    browser.config.window_width = window_width
+    browser.config.window_height = window_height
+
+    yield
+
+
